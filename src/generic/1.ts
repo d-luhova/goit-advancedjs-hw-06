6682d405e2dc7d7938c0daf527345c0fd62557d4
Daria Luhova <missis.lugowaya@gmail.com>
2025-06-19 10:08:29 +0200

    update

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,15 +3,14 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise () {
+function getPromise<T>(): Promise<T[]> {
   return new Promise((resolve) => {
-    resolve(['Text', 50]);
+    resolve(['Text', 50] as T[]);
   });
 }
 
-getPromise()
-.then((data) => {
-  console.log(data);
+getPromise<string | number>().then((data) => {
+  console.log(data); // ['Text', 50]
 });
 
 export {};
